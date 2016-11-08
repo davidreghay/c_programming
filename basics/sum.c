96bf7cf51bfa50f2c9308b112bd2dfbf453e1087
David Reghay <david.reghay@gmail.com>
2016-11-08 00:34:06 -0500

    Pass argv to usage and improve formatting

--- basics/sum.c
+++ basics/sum.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void print_usage(void);
+void print_usage(char** args);
 
 int main(int argc, char** argv) {
 	if(argc != 3) {
-		print_usage();
+		print_usage(argv);
 		exit(1);
 	}
 	int a = atoi(argv[1]),
 		b = atoi(argv[2]);
-	printf("%d is the sum", a + b);
+	printf("%d is the sum\n", a + b);
 }
 
-void print_usage(void) {
-	printf("Usage: \tsum NUM1 NUM2, where NUM1 and NUM2 can "
-		"safely be converted to integer values.");
+void print_usage(char** args) {
+	printf("Usage: \t%s NUM1 NUM2, where NUM1 and NUM2 can "
+		"safely be converted to integer values.\n", args[0]);
 }
